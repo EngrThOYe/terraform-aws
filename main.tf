c1f050124dfe27c4235c0c3e7a48ab84acc578fa
EngrThOYe <adetoy_4real@yahoo.com>
2022-04-13 18:34:00 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+    region = "us-east-1"
+
+  
+}
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = "10.20.0.0/16"
+    tags = {
+      "Name" = "development"
+    }
+  
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = "10.20.20.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+      "Name" = "subnet-1-dev"
+    }
+  
+}
+
+
+output "dev-vpc-id" {
+    value = aws_vpc.development-vpc.id
+  
+}
+
+output "dev-subnet-id" {
+    value = aws_subnet.dev-subnet-1.id
+  
+}
